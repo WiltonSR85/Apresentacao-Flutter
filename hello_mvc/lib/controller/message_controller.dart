6b389d6f181fa3e45589c7bcc2f8b637d75b5da9
WiltonSR85 <wiltonsilvarodrigues4321@email.com>
2025-08-12 14:16:17 -0300

    refactor: Adequando corretamente ao padrão MVC

--- hello_mvc/lib/controller/message_controller.dart
+++ hello_mvc/lib/controller/message_controller.dart
@@ -1,7 +1,11 @@
 import '../model/message_model.dart';
 
 class MessageController {
-  final MessageModel _model = MessageModel();
+  final MessageModel _model;
+
+  MessageController() : _model = MessageModel(
+    'Olá mundo, bem-vindo ao Flutter MVC! Esta é uma aplicação simples que demonstra o padrão MVC (Model-View-Controller) no Flutter.'
+  );
 
   String getMessage() {
     return _model.message;
--- hello_mvc/lib/model/message_model.dart
+++ hello_mvc/lib/model/message_model.dart
@@ -1,5 +1,4 @@
 class MessageModel {
-  String message = 
-  '''Olá mundo, bem-vindo ao Flutter MVC! 
-Esta é uma aplicação simples que demonstra o padrão MVC (Model-View-Controller) no Flutter. ''';
+  final String message;
+  MessageModel(this.message);
 }
